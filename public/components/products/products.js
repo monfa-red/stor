9f3353741f8ce7512a2222fec4dc5ff813333959
Abbas Monfared <a@monfa.red>
2015-07-29 23:57:38 +0000

    Basic angular integration

--- public/components/products/products.js
+++ public/components/products/products.js
@@ -2,10 +2,34 @@ angular
     
     .module('storeApp.products', [])
 
-    .controller('ProductsController', productsController)
+    .service('storeApp.productService', function() {
+      this.working = "Yes!"
+    })
 
+    .controller('ProductsController', ProductsController)
 
-    function productsController() {
+    .directive('productList', function() {
+      return {
+        restrict: "E",
+        template: `<h5>list of products</h5>
+                    <ul>
+                      <li ng-repeat="product in productList.products">
+                        <h6>{{ product.title }}</h6>
+                        <p>{{ product.name }}</p>        
+                      </li>
+                    </ul>`,
+        controller: function() {
+          this.products = [{
+            title: "testTitle",
+            name: "tastName"
+          }];
+          console.log(this.products);
+        },
+        controllerAs: "productList"
+      }
+    });
+
+    function ProductsController() {
 
     }
 
