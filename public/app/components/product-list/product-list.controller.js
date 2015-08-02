6a5184e8b8e3c2735183590084eabb101eae5b18
Abbas Monfared <a@monfa.red>
2015-08-01 21:36:48 -0400

    Angular New Router Stuff

--- public/app/components/product-list/product-list.controller.js
+++ public/app/components/product-list/product-list.controller.js
@@ -5,34 +5,12 @@
 
     .module('app.productList')
 
-    .controller('ProductListController', ProductListController)
+    .controller('ProductListController', ProductListController);
 
-    .directive('productNameList', function() {
-      return {
-        restrict: "E",
-        template: `<h5>list of products</h5>
-                    <ul>
-                      <li ng-repeat="product in productNameList.products">
-                        <h6>{{ product.title }}</h6>
-                        <p>{{ product.name }}</p>
-                      </li>
-                    </ul>`,
-        controller: function() {
-          this.products = [{
-            title: "testTitle",
-            name: "tastName"
-          }];
-          console.log(this.products);
-        },
-        controllerAs: "productNameList"
-      }
-    });
 
     function ProductListController() {
 
     }
-
-
 
 
 })()
