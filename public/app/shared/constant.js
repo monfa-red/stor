2b48dc4fa261f743bed5f5ee1d8bd0c9beb2b932
Abbas Monfared <a@monfa.red>
2015-08-06 17:22:25 -0400

    ng: Main Constant added

---
+++ public/app/shared/constant.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+
+    .module('app')
+
+    .constant('constants',
+      {
+        appName: "Stor",
+        appDescription: "Open-source Store App"
+      }
+    );
+
+})();
