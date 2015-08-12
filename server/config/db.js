3a546ade4383efce7d8aceb62cf03b12b57b332b
Abbas Monfared <a@monfa.red>
2015-08-12 17:58:10 -0400

    cleaned and debuged mongoose db connection module

---
+++ server/config/db.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Dependencies
+ */
+
+var config = require('./config'),
+  chalk = require('chalk'),
+  mongoose = require('mongoose');
+
+
+
+/**
+ * Initialize Mongoose db connection
+ */
+
+module.exports.connect = function(cb) {
+
+  mongoose
+
+    // Connect to db
+    .connect(config.db.uri, config.db.option)
+
+    // Set debug mode if required
+    .set('debug', config.db.debug)
+
+    // handle connection error/success
+    .connection
+      .on('error', connectionError)
+      .on('open', connectionSucess);
+
+
+  function connectionError(err) {
+    console.error(chalk.bgRed('Could not connect to db'), chalk.red(err));
+  }
+
+  function connectionSucess(cb) {
+    // Some typhography!
+    console.log("   " + chalk.bgBlue('    Connected to DB from server.js    '))
+    // Call success callback if required
+    if (cb) cb(db);
+  }
+
+}
