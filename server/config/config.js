64e14c355faae3daa00dc5f1d1295a0ca59f66f7
Abbas Monfared <a@monfa.red>
2015-08-12 18:56:59 -0400

    Initialized the config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,23 @@
 module.exports = {
-	db: 'mongodb://localhost/mn-s'
+  db: {
+    uri:
+		process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mn-s',
+    options: {
+      // user: '',
+      // pass: ''
+    },
+    // Enable mongoose debug mode
+    debug: process.env.MONGODB_DEBUG || false
+  },
+  // log: {
+  //   // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+  //   format: 'dev',
+  //   // Stream defaults to process.stdout
+  //   // Uncomment to enable logging to a log on the file system
+  //   options: {
+  //     //stream: 'access.log'
+  //   },
+	app: {
+    title: 'Stor'
+  },
 }
