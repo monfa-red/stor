d325b46589b8b14e39aae721b7d8cac39b55161b
Abbas Monfared <a@monfa.red>
2015-08-14 16:57:34 -0400

    changing controller syntax

--- server/controllers/products.server.controller.js
+++ server/controllers/products.server.controller.js
@@ -7,131 +7,160 @@ var mongoose = require('mongoose'),
   Product = mongoose.model('Product'),
   errorHandler = require('../controllers/errors.server.controller');
 
+
 /**
  * Create a product
  */
 
 module.exports = {
 
-  create: function (req, res) {
-    var product = new Product(req.body);
+  create: create,
 
-    // console.log(req)
-    // console.log(req.body)
-    // TODO: Product authour needs to be set
-    // product.user = req.user;
+  read: read,
 
-    product.save(function (err) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(product);
-      }
-    });
-  },
+  update: update,
 
-  /**
-   * Show the current product
-   */
-  read: function (req, res) {
-    // res.json(req.params.productId);
-    Product
-      .findOne({ 'nameId': req.params.productId })
-       // TODO: only return necessary data
-      .populate('author')
-      .exec(function (err, product) {
-        if (err) {
-          return res.status(400).send({
-            message: errorHandler.getErrorMessage(err)
-          });
-        } else {
-          res.json(product);
-        }
-      });
-    // console.log(req.params.productId)
-  },
+  delete: delete,
 
-  /**
-   * Update a product
-   */
-  update: function (req, res) {
-    var product = req.product;
+  list: list,
 
-    product.title = req.body.title;
-    product.content = req.body.content;
+  productByID: productByID
 
-    product.save(function (err) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(product);
-      }
-    });
-  },
+}
 
-  /**
-   * TODO: definitly change this!
-   * Delete an product
-   */
-  delete: function (req, res) {
-    // var product = req.product;
-    // var product = req.params.productId;
-    // console.log(product)
 
-    Product.remove({ _id: req.params.productId }, function (err, result) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(result);
-      }
-    });
-  },
 
-  /**
-   * List of Articles
-   */
-  // TODO: .populate('user', 'displayName')
 
-  list: function (req, res) {
-    Product.find().sort('-created').exec(function (err, products) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(products);
-      }
-    });
-  },
 
-  /**
-   * Product middleware
-   */
-  productByID: function (req, res, next, id) {
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+function create(req, res) {
+
+  var product = new Product(req.body);
+
+  product.save(function (err) {
+    if (err) {
       return res.status(400).send({
-        message: 'Product is invalid'
+        message: errorHandler.getErrorMessage(err)
       });
+    } else {
+      res.json(product);
     }
+  })
 
-    Product.findById(id).populate('user', 'displayName').exec(function (err, product) {
+};
+
+
+/**
+ * Show the current product
+ */
+
+function read(req, res) {
+  // res.json(req.params.productId);
+  Product
+    .findOne({ 'nameId': req.params.productId })
+     // TODO: only return necessary data
+    .populate('author')
+    .exec(function (err, product) {
       if (err) {
-        return next(err);
-      } else if (!product) {
-        return res.status(404).send({
-          message: 'No product with that identifier has been found'
+        return res.status(400).send({
+          message: errorHandler.getErrorMessage(err)
         });
+      } else {
+        res.json(product);
       }
-      req.product = product;
-      next();
+    });
+
+};
+
+
+/**
+ * Update a product
+ */
+
+function update(req, res) {
+  var product = req.product;
+
+  product.title = req.body.title;
+  product.content = req.body.content;
+
+  product.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(product);
+    }
+  });
+};
+
+
+
+/**
+ * TODO: definitly change this!
+ * Delete an product
+ */
+
+function delete(req, res) {
+  // var product = req.product;
+  // var product = req.params.productId;
+
+  Product.remove({ _id: req.params.productId }, function (err, result) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(result);
+    }
+  });
+
+};
+
+
+/**
+ * List of Articles
+ */
+// TODO: .populate('user', 'displayName')
+
+function list(req, res) {
+
+  Product.find().sort('-created').exec(function (err, products) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(products);
+    }
+  });
+
+};
+
+
+
+
+/**
+ * Product middleware
+ */
+function productByID(req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Product is invalid'
     });
   }
 
-}
+  Product.findById(id).populate('user', 'displayName').exec(function (err, product) {
+    if (err) {
+      return next(err);
+    } else if (!product) {
+      return res.status(404).send({
+        message: 'No product with that identifier has been found'
+      });
+    }
+    req.product = product;
+    next();
+  });
+
+};
