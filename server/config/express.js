a102a87d03d5599ccb10685c474fcbbbb6419e17
Abbas Monfared <a@monfa.red>
2015-08-17 04:33:22 -0400

    finialized express module in es2015

--- server/config/express.js
+++ server/config/express.js
@@ -1,24 +1,17 @@
 'use strict';
 
+// session = require('express-session'),
+// MongoStore = require('connect-mongo')(session),
+// multer = require('multer'),
+// favicon = require('serve-favicon'),
+// compress = require('compression'),
+// methodOverride = require('method-override'),
+// helmet = require('helmet'),
+// flash = require('connect-flash'),
+
 /**
  * Module dependencies.
  */
-// var config = require('../config'),
-//   express = require('express'),
-//   morgan = require('morgan'),
-//   bodyParser = require('body-parser'),
-//   session = require('express-session'),
-//   MongoStore = require('connect-mongo')(session),
-//   multer = require('multer'),
-//   favicon = require('serve-favicon'),
-//   compress = require('compression'),
-//   methodOverride = require('method-override'),
-//   cookieParser = require('cookie-parser'),
-//   helmet = require('helmet'),
-//   flash = require('connect-flash'),
-//   consolidate = require('consolidate'),
-//   path = require('path');
-
 import express from 'express';
 import path from 'path';
 // import favicon from 'serve-favicon';
@@ -26,28 +19,49 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import consolidate from 'consolidate';
-import config from '../config/config';
+import config from './config';
 
 
 /**
- * A list of all Exported methods
+ * Export list
  */
-
 export default {
 
-  initMiddlewares,
-
-  initViewEngine,
-
-  initSession,
-
-  initModulesClientRoutes,
-
-  initModulesServerRoutes,
-
-  initErrorRoutes,
-
   init
+
+};
+
+
+function init(db) {
+
+  // Initialize express app
+  let app = express();
+
+  // local variables this.initLocalVariables(app);
+
+  // Initialize Express middleware
+  initMiddlewares(app);
+
+  // Initialize Express view engine
+  initViewEngine(app);
+
+  // Express session initSession(app, db);
+  // Modules configuration initModulesConfiguration(app);
+  // Helmet security headers initHelmetHeaders(app);
+
+  // Initialize modules static client routes
+  initModulesClientRoutes(app);
+
+  // modules server authorization policies initModulesServerPolicies(app);
+
+  // Initialize modules server routes
+  initModulesServerRoutes(app);
+
+  // Initialize error routes
+  initErrorRoutes(app);
+
+  // return Initialize express app for bootstraping
+  return app;
 
 };
 
@@ -55,13 +69,13 @@ export default {
 /**
  * Initialize application middlewares
  */
-
 function initMiddlewares(app) {
 
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
+  app
+    .use(logger('dev'))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(cookieParser());
 
 };
 
@@ -69,7 +83,6 @@ function initMiddlewares(app) {
 /**
  * Configure view engine
  */
-
 function initViewEngine(app) {
 
   // Set swig as the template engine and assign it to .html files
@@ -84,18 +97,17 @@ function initViewEngine(app) {
 /**
  * Configure Express session
  */
-
 function initSession(app) {
 
 };
 
+
 /**
  * Setting the client-side static folder
  */
-
 function initModulesClientRoutes(app) {
 
-  app.use(express.static(path.join(__dirname, '../', 'public')));
+  app.use(express.static(path.join(__dirname, '../../', 'public')));
 
 };
 
@@ -103,7 +115,6 @@ function initModulesClientRoutes(app) {
 /**
  * Configure the modules server routes
  */
-
 function initModulesServerRoutes(app) {
 
   // Globbing routing files
@@ -115,8 +126,9 @@ function initModulesServerRoutes(app) {
     // import routePath;
     // routePath(app);
     require(path.resolve(routePath))(app);
-    console.log('%s imported', routePath);
+    console.log('Mounted: %s', routePath);
   });
+    // require('../../server/routes/products.server.routes')(app)
 
 };
 
@@ -124,7 +136,6 @@ function initModulesServerRoutes(app) {
 /**
  * Configure error handling
  */
-
 function initErrorRoutes(app) {
 
   app.use(function (err, req, res, next) {
@@ -132,60 +143,18 @@ function initErrorRoutes(app) {
     if (!err) {
       return next();
     }
-
     // Log it
     console.error(err.stack);
 
     // Redirect to error page
-    res.redirect('/server-error');
+    res.redirect('/error-page');
   });
 
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-};
-
-
-
-
-function init(db) {
-  // Initialize express app
-  var app = express();
-
-  // Initialize local variables
-  // this.initLocalVariables(app);
-
-  // Initialize Express middleware
-  this.initMiddlewares(app);
-
-  // Initialize Express view engine
-  this.initViewEngine(app);
-
-  // Initialize Express session
-  // this.initSession(app, db);
-
-  // Initialize Modules configuration
-  // this.initModulesConfiguration(app);
-
-  // Initialize Helmet security headers
-  // this.initHelmetHeaders(app);
-
-  // Initialize modules static client routes
-  this.initModulesClientRoutes(app);
-
-  // Initialize modules server authorization policies
-  // this.initModulesServerPolicies(app);
-
-  // Initialize modules server routes
-  this.initModulesServerRoutes(app);
-
-  // Initialize error routes
-  this.initErrorRoutes(app);
-
-  return app;
+  // app.use(function(req, res, next) {
+  //   var err = new Error('Not Found');
+  //   err.status = 404;
+  //   next(err);
+  // });
 
 };
