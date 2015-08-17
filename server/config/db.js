dbed6f25743eda784601bf15f7cddc9c30fa495d
Abbas Monfared <a@monfa.red>
2015-08-17 04:32:52 -0400

    finialized db module in es2015

--- server/config/db.js
+++ server/config/db.js
@@ -1,51 +1,36 @@
 'use strict';
 
 /**
- * Dependencies
+ * Module dependencies
  */
-
-var config = require('./config'),
-  chalk = require('chalk'),
-  mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+import config from './config';
+import events from './events';
 
 
 /**
- * Exported methods
+ * Export list
  */
- 
 export default {
+
   connect
-}
+
+};
+
 
 /**
- * Initialize Mongoose db connection
+ * Initialize Mongoose and database connection
  */
-
 function connect(callback) {
 
   mongoose
-
-    // Connect to db
     .connect(config.db.uri, config.db.option)
-
     // Set debug mode if required
     .set('debug', config.db.debug)
-
-    // handle connection error/success
-    .connection
-      .on('error', connectionError)
-      .on('open', connectionSucess);
-
-
-  function connectionError(err) {
-    console.error(chalk.bgRed('Could not connect to db'), chalk.red(err));
-  }
-
-  function connectionSucess(callback) {
-    // Some typhography!
-    console.log("   " + chalk.bgBlue('    Connected to DB from server.js    '))
-    // Call success callback if required
-    if (callback) callback(db);
-  }
+      // handle connection error/success
+      .connection
+        .on('error', events.db.error)
+        .on('open', () => events.db.success(callback));
 
 }
