ecf03810792681dcabbb3dbeadc2dea5fd81ceba
Abbas Monfared <a@monfa.red>
2015-08-19 13:18:10 -0400

    Added sample ssl keys

---
+++ server/config/lib/keys/certificate.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+import fs from 'fs';
+import config from '../../config';
+
+
+/**
+ * Export key and cert object
+ */
+export default certificate;
+
+
+/**
+ * Read ssl keys and pass it to an object
+ */
+function certificate() {
+
+  if (config.https.port) {
+    return {
+      key: fs.readFileSync(config.https.ssl.key),
+      cert: fs.readFileSync(config.https.ssl.cert)
+    };
+  }
+
+};
