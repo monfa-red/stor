78f8bcab736342612cb4cd8e8f5ad3c0a5a065ed
Abbas Monfared <a@monfa.red>
2015-08-28 18:12:58 -0400

    Added google strategy

---
+++ server/modules/users/auth/google.js
@@ -0,0 +1,52 @@
+/**
+ * Module dependencies
+ */
+ import passport from 'passport';
+ import {OAuth2Strategy} as GoogleStrategy from 'passport-google-oauth';
+ import mongoose from 'mongoose';
+
+
+ /**
+  * Get the User model
+  */
+ let User = mongoose.model('User');
+
+
+
+// Use google strategy
+passport.use(new GoogleStrategy({
+    clientID: config.strategies.google.clientID,
+    clientSecret: config.strategies.google.clientSecret,
+    callbackURL: config.strategies.google.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => {
+
+    User
+      .findOne({
+        'google.id': profile.id
+      })
+      .exec((err, user) => {
+        if (err) return done(err);
+
+        if (user) return done(null, user);
+
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          role: 'user',
+          provider: 'google',
+          google: profile._json
+        });
+
+        user.save(err => {
+          if (err) {
+            return done(null, false, {
+              message: 'Google login failed, email already used by other login strategy'
+            });
+          }
+          return done(null, user);
+        });
+      });
+
+  }
+));
